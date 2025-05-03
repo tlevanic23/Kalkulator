ffea2277ccd5f35e11e6403c015e54f0e7d34928
tlevanic23 <tlevanic23@student.foi.hr>
2025-05-03 20:19:38 +0200

    Dodano brisanje trake za unos nakon ispisanog rezultata

--- Kalkulator/Kalkulator.cs
+++ Kalkulator/Kalkulator.cs
@@ -19,6 +19,7 @@ namespace Kalkulator
 
         int prviBroj, drugiBroj;
         int brojac = 0;
+        char operacija;
 
         private void txt_TextChanged(object sender, EventArgs e)
         {
@@ -27,76 +28,112 @@ namespace Kalkulator
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (brojac == 0) prviBroj = 1;
+            else drugiBroj = 1;
+                brojac++;
             txt.Text +=" 1";
         }
 
         private void btn2_Click(object sender, EventArgs e)
         {
+            if (brojac == 0) prviBroj = 2;
+            else drugiBroj = 2;
+            brojac++;
             txt.Text += " 2";
         }
 
         private void btn3_Click(object sender, EventArgs e)
         {
+            if (brojac == 0) prviBroj = 3;
+            else drugiBroj = 3;
+            brojac++;
             txt.Text += " 3";
         }
 
         private void btn4_Click(object sender, EventArgs e)
         {
+            if (brojac == 0) prviBroj = 4;
+            else drugiBroj = 4;
+            brojac++;
             txt.Text += " 4";
         }
 
         private void btn5_Click(object sender, EventArgs e)
         {
+            if (brojac == 0) prviBroj = 5;
+            else drugiBroj = 5;
+            brojac++;
             txt.Text += " 5";
 
         }
 
         private void btn6_Click(object sender, EventArgs e)
         {
+            if (brojac == 0) prviBroj = 6;
+            else drugiBroj = 6;
+            brojac++;
             txt.Text += " 6";
 
         }
 
         private void btn7_Click(object sender, EventArgs e)
         {
+            if (brojac == 0) prviBroj = 7;
+            else drugiBroj = 7;
+            brojac++;
             txt.Text += " 7";
 
         }
 
         private void btn8_Click(object sender, EventArgs e)
         {
+            if (brojac == 0) prviBroj = 8;
+            else drugiBroj = 8;
+            brojac++;
             txt.Text += " 8";
 
         }
 
         private void btn10_Click(object sender, EventArgs e)
         {
+            if (brojac == 0) prviBroj = 9;
+            else drugiBroj = 9;
+            brojac++;
             txt.Text += " 9";
         }
 
         private void btnPlus_Click(object sender, EventArgs e)
         {
+            operacija = '+';
             txt.Text += " +";
         }
 
         private void btnMinus_Click(object sender, EventArgs e)
         {
+            operacija = '-';
             txt.Text += " -";
         }
 
         private void btnTimes_Click(object sender, EventArgs e)
         {
+            operacija = '*';
             txt.Text += " *";
         }
 
         private void btnDivide_Click(object sender, EventArgs e)
         {
+            operacija = '/';
             txt.Text += " /";
         }
 
-        private void btnEqual_Click(object sender, EventArgs e)
+        private async void btnEqual_Click(object sender, EventArgs e)
         {
-            txt.Text += " =";
+            if (operacija == '+') txt.Text = (prviBroj + drugiBroj).ToString();
+            if (operacija == '-') txt.Text = (prviBroj - drugiBroj).ToString();
+            if (operacija == '*') txt.Text = (prviBroj * drugiBroj).ToString();
+            if (operacija == '/') txt.Text = (prviBroj / drugiBroj).ToString();
+            await Task.Delay(3000); 
+            txt.Text = string.Empty; 
         }
     }
 }
